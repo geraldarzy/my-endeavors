fd64e748ca04eeb826a864cf538a39713631e7d3
geraldarzy <geraldarzola@gmail.com>
2021-02-07 04:05:50 -0500

    add helper methods

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,14 +12,17 @@ class UserController < ApplicationController #can either inherit from SinatraBas
 
    post '/signup' do #/users
         user = User.new(params[:user])
-        if user.password.blank? || user.username.empty?
+        if user.password.blank? || user.username.empty? #checks if input is valid
             @error = "Username and/or password can not be blank. Please enter a valid username and password."
             erb :"users/signup"
+        elsif User.find_by(username: user.username)     # checks if username is taken
+            @error = "Username is already taken, please choose a different username."
+            erb :"users/signup"
         else
-            user.save
-            session[:id] = user.id
-            redirect "/users/#{user.username}"
+            user.save                                   #finally saves user if passes all tests
+            session[:id] = user.id                      #user.id is set to a non-stateless variable
+            redirect "/users/#{user.username}"          #redirects to home page customed to user
         end
         
-   end
+    end
 end
