142e214cb685f2dce8dbd653a924121e36a23be1
geraldarzy <geraldarzola@gmail.com>
2021-02-07 05:10:09 -0500

    add login functions

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+    #users login
+    get '/login' do
+        erb :"users/login"
+    end
+
+    post '/login' do
+       # puts params
+       # puts params[:user]
+       # puts params[:user][:username]
+       if params[:user][:username].empty? || params[:user][:password].blank?
+            @error = "Username and/or password can not be blank. Please enter a valid username and password."
+            erb :"users/login"
+       else
+            if  user = User.find_by(username: params[:user][:username])
+                if user.authenticate(params[:user][:password])
+                    session[:id] = user.id
+                    redirect "/users/#{user.username}"
+                else
+                    @error = "Incorrect password, please try again."
+                    erb :"users/login"
+                end
+            else
+                @error = "Account not found, please try again."
+                erb :"users/login"
+            end
+        end
+    end
+
+    #users logout
+end
--- config.ru
+++ config.ru
@@ -5,4 +5,5 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
 end
 
 use UserController
+use SessionsController
 run ApplicationController
