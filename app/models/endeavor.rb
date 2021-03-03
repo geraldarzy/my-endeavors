1ccc2c14b3ce84088c6cb827010ad6c837364596
geraldarzy <geraldarzola@gmail.com>
2021-03-03 18:28:53 -0500

    add endeavor validations with AR

--- app/models/endeavor.rb
+++ app/models/endeavor.rb
@@ -1,4 +1,19 @@
 
 class Endeavor < ActiveRecord::Base
+
+
+   validates :title, presence: true
+   validates :description, presence: true
+
+   
    belongs_to :user
+
+   def get_usable_link
+      self.pic.gsub(/dl=0/, "raw=1")
+   end
+
+   def picture_available?
+      !!self.pic
+   end
+
 end
