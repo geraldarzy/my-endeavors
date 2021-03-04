ef416161ae6f44e91c3a0d1943ba62e6c1ec55fa
geraldarzy <geraldarzola@gmail.com>
2021-03-03 19:01:55 -0500

    refactor user sign up with AR Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 
 
 class User < ActiveRecord::Base
+    validates :username, presence:true
+    validates :username, uniqueness:true
+    validates :password, presence:true
+    
     has_secure_password                 #bcrypt, salts the password we give in, also why our password column is 'password_digest'
     has_many :endeavors
 end
